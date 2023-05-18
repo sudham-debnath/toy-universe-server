8d6b9ad2a1630ff487521a543eddb33688220fde
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-05-18 13:42:34 +0600

    Initial mongodb setup

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+//pitushdebnath0
+//hjLNfITjdjHkEZMY
 
 app.get('/', (req, res) => {
     res.send('Toy Universe Server is running successfully')
