a24c44a78120f39372d1bcc17de0ecacb2111ab1
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-05-21 20:28:46 +0600

    Finish All Api Making

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    // client.close();
   }
 }
 run().catch(console.dir);
