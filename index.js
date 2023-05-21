033090a47dffb34644c7471232dec47e2aa8d791
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-05-21 15:48:44 +0600

    Making Book details and my toy API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,9 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 //MongoDB
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.apksail.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,54 +27,70 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
 
-    
-    const toyCollection = client.db("toyManager").collection("toy")
-
+    const toyCollection = client.db("toyManager").collection("toy");
 
     // Insert (Create) a toy to database
     app.post("/upload-toy", async (req, res) => {
-      const data = req.body
+      const data = req.body;
       console.log(data);
-      const result = await toyCollection.insertOne(data)
-      res.send(result)
-    })
-    
+      const result = await toyCollection.insertOne(data);
+      res.send(result);
+    });
+
     // Get (Read) a toy from database
-    app.get("/all-toys", async (req, res) => { 
+    app.get("/all-toys", async (req, res) => {
       const toys = toyCollection.find();
       const result = await toys.toArray();
       res.send(result);
-    })
+    });
 
-    
     //Update a toy data using id
-    app.patch("/toy/:id", async (req, res) => { 
+    app.patch("/toy/:id", async (req, res) => {
       const id = req.params.id;
       const updateToyData = req.body;
-      const filter = { _id: new ObjectId(id) }
+      const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          ...updateToyData
-        }
-      }
-      const result = await toyCollection.updateOne(filter, updatedDoc)
-      res.send(result)
-    })
-
+          ...updateToyData,
+        },
+      };
+      const result = await toyCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     //Delete a toy data using id
     app.delete("/toy/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const result = await toyCollection.deleteOne(filter);
+        res.send(result);
+      } catch (err) {
+        console.log(err.message);
+      }
+    });
+
+    //Book Details
+    app.get("/toy/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id) }
-      const result = await toyCollection.deleteOne(filter)
-      res.send(result)
-    })
+      const filter = { _id: new ObjectId(id) };
+      const data = await toyCollection.findOne(filter);
+      res.send(data);
+    });
 
-
-
+    //My Toys
+    app.get("/myToys/:email", async (req, res) => {
+      // console.log(req.params.email);
+      const result = await toyCollection
+        .find({ seller_email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -83,13 +98,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('Toy Universe Server is running successfully on port 5000')
+app.get("/", (req, res) => {
+  res.send("Toy Universe Server is running successfully on port 5000");
 });
 
 app.listen(port, () => {
-    console.log(`Toy Universe Server is running on port : ${port}`);
+  console.log(`Toy Universe Server is running on port : ${port}`);
 });
